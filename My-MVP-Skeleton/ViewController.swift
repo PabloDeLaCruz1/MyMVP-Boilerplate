c962664d147d7e3352d6334d2f165b2716897d78
PabloDeLaCruz1 <pablo.de.la.cruz.pro@gmail.com>
2022-03-03 00:16:25 -0500

    Initial Commit

---
+++ My-MVP-Skeleton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My-MVP-Skeleton
+//
+//  Created by Pablo De La Cruz on 3/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
